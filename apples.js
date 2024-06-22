1462e8d709aab5854109a6906995de73357a3e77
theplantpsychologist@gmail.com <theplantpsychologist>
2024-06-22 17:35:33 -0400

    added comments

--- apples.js
+++ apples.js
@@ -1,18 +1,2 @@
-const fs = require('fs');
 
-// Function to read the file and feed it into readCp()
-function readFileAndFeedToReadCp(filePath) {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        
-        testcp = readCpFile(data); // Call the readCp() function with the file content as an argument
-        displayCp(testcp, 10, 10, 590, 590)
-    });
-}
-
-// Usage: Provide the path to the text file as an argument
-readFileAndFeedToReadCp('test.cp');
 
